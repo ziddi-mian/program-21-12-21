cfabe546cdadcbc818701a1f1545038088862e4d
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-21 17:43:53 +0500

    js

--- script.js
+++ script.js
@@ -97,8 +97,23 @@ dt = new Date(2015, 10, 1);
 console.log(ISO_numeric_date(dt));
 
 
+// Write a JavaScript function to get English ordinal suffix for the day of the month, 2 characters (st, nd, rd or th.). Go to the editor
+// Test Data :
+// dt = new Date(2015, 10, 1);
+// console.log(english_ordinal_suffix(dt));
+// "1st"
 
 
+function english_ordinal_suffix(dt)
+  {
+    return dt.getDate()+(dt.getDate() % 10 == 1 && dt.getDate() != 11 ? 'st' : (dt.getDate() % 10 == 2 && dt.getDate() != 12 ? 'nd' : (dt.getDate() % 10 == 3 && dt.getDate() != 13 ? 'rd' : 'th'))); 
+  }
+
+dt = new Date();
+console.log(english_ordinal_suffix(dt));
+
+dt = new Date(2015, 10, 1);
+console.log(english_ordinal_suffix(dt));
 
 
 
