74562f8abb0a0386f1db000b76048ca7a1c53852
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-21 17:41:41 +0500

    nw hs

--- script.js
+++ script.js
@@ -57,3 +57,24 @@ console.log(short_Days(dt));
 dt = new Date(2015, 10, 1);
 console.log(short_Days(dt));
 
+
+// Write a JavaScript function to get a full textual representation of the day of the week (Sunday through Saturday).   
+// Test Data :
+// dt = new Date(2015, 10, 1);
+// console.log(long_Days(dt));
+// "Sunday"
+
+Date.longDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function long_Days(dt)
+    { 
+      return Date.longDays[dt.getDay()]; 
+    }
+
+dt = new Date();
+console.log(long_Days(dt));
+
+dt = new Date(2015, 10, 1);
+console.log(long_Days(dt));
+
+
