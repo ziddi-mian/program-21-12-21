82894cef49998ad778f90883f8affff2e0495810
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-21 17:43:00 +0500

    js

--- script.js
+++ script.js
@@ -78,3 +78,41 @@ dt = new Date(2015, 10, 1);
 console.log(long_Days(dt));
 
 
+
+// Write a JavaScript function to get ISO-8601 numeric representation of the day of the week (1 (for Monday) to 7 (for Sunday)).   
+// Test Data :
+// dt = new Date(2015, 10, 1);
+// console.log(ISO_numeric_date(dt));
+// 7
+
+function ISO_numeric_date(dt)
+{
+  return (dt.getDay() === 0 ? 7 : dt.getDay()); 
+}
+
+dt = new Date();
+console.log(ISO_numeric_date(dt));
+
+dt = new Date(2015, 10, 1);
+console.log(ISO_numeric_date(dt));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
